708ee6fcadfb0c9ce5b92c71a3092063a8c522b7
salmane-ben <salmane-ben@live.com>
2021-03-24 23:58:52 +0100

    going home

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ const Hero = ({appType, tagLine, description, mainActionText, extraActionText}) 
     <div id="product">
       <div style={{textShadow:'0px 1px 1px gray'}} className="flex flex-col items-center justify-start font-sans min-h-96 bg-gray-50 lg:pt-10 lg:pb-20 lg:bg-hero lg:bg-cover">
         <div>
-          <p className="p-3 pt-12 text-lg font-bold text-gray-500 lg:text-gray-300">{appType}</p>
+          {/* <p className="p-3 pt-12 text-lg font-bold text-gray-500 lg:text-gray-300">{appType}</p> */}
         </div>
         <div>
           <p className="p-2 text-4xl font-bold text-center text-blue-800 lg:mx-auto lg:w-4/6 lg:text-5xl lg:text-gray-100">
