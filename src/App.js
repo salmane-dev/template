c12f9cd02c0dd7e3f9badc376b7a48e4275efef3
salmane-ben <salmane-ben@live.com>
2021-04-03 18:08:23 +0100

    json implemented

--- src/App.js
+++ src/App.js
@@ -25,46 +25,13 @@ import Rectangle_4 from './assets/online-shopping.png';
 import Rectangle_5 from './assets/Rectangle_5.png';
 import Rectangle_6 from './assets/action.png'; 
 import Rectangle_7 from './assets/action.png'; 
+const data = require('./data.json'); 
  
 
 
 function App() {
+ 
 
-  const data = {
-    hero:{
-      appType: 'landing page',
-      tagLine: 'Welcome to '+ window.location.host,
-      description: 'best money-saving offers on the Web',
-      mainActionText: 'Contact us',
-      extraActionText: 'Unsubscribe',
-      sitename: 'Yourwebsite.com', 
-      primarycolor: '#ea580c',
-      primarycolorhover: '#c2410c',
-      hero_bg: 'https://images.unsplash.com/photo-1574607383363-5b5f1747b37b',
-      action_bg: 'https://images.unsplash.com/photo-1534536281715-e28d76689b4d',
-      unsubscripbe_bg:'https://images.unsplash.com/photo-1533289408336-ac92d0dbf036',
-    },
-    step1: {
-      title: 'Create an account',
-      heading: ' dedicated to finding the best money-saving offers on the Web and delivering them directly to you, the consumer.      Business profiles contain comprehensive information on contacts, products, services, trade experience and technology.',
-      //description: 'An account is created with your email and a desired password',
-      img: Rectangle_3,
-      alternate: false,
-    },
-    step2: {
-      title: 'Explore while shopping',
-      heading: 'We strive to provide the highest quality and lowest prices available to our customers, \n   resulting in one of the best satisfaction ratings in the industry.    If you\'re not already receiving our money-saving offers and web-only specials,contact us today.',
-      description: ' ',
-      img: Rectangle_4,
-      alternate: true,
-    },
-    bottomLead: {
-      actionText: 'Join us now.',
-      description: 'To receive our exclusive e-mail offers from '+ window.location.host +' name companies, please complete the form bellow.',
-      mainActionText: 'Submit',
-      extraActionText: '',
-    },
-  }
   return (
     
     <Router>
@@ -86,7 +53,6 @@ function App() {
                     primarycolor={data.hero.primarycolor}
                   />
                   
-                  {/* <div id="divider" className="rounded-full ring-2 ring-gray-200 lg:w-1/2 lg:mx-auto "></div> */}
                   
                   <div id="faq" className="pt-10 mb-20 text-4xl font-semibold text-center text-blue-800 lg:font-bold">Welcome to {data.hero.sitename} ! </div>
                   
--- src/components/Step.js
+++ src/components/Step.js
@@ -8,14 +8,11 @@ const Step = ({title, heading, description, img, alternate}) => {
         <div className={ alternate? row : rowReverse}>
           
           <div className="lg:w-2/5 lg:h-4/6 lg:-ml-20 lg:-mt-10lg:flex lg:flex-col lg:justify-center lg:items-start min-h-48 ">
-            
-            {/* <p className="pb-5 text-2xl font-semibold text-center text-orange-400 lg:pb-0 lg:-mb-3 lg:text-lg lg:font-bold lg:text-left">{title}</p> */}
-            <p className="p-5 text-lg font-normal text-center text-blue-800 lg:pb-4 lg:text-xl lg:leading-loose lg:pl-0 lg:font-normal lg:text-left">{heading}</p>
-            <p className="p-5 pb-0 pl-10 pr-10 text-3xl leading-10 text-center text-gray-400 lg:w-5/6 lg:pb-0 lg:text-3xl lg:text-left lg:p-0 lg:pl-0 lg:pr-0">{description}</p>
+              <p className="p-5 text-lg font-normal text-center text-blue-800 lg:pb-4 lg:text-xl lg:leading-loose lg:pl-0 lg:font-normal lg:text-left">{heading}</p>
+              <p className="p-5 pb-0 pl-10 pr-10 text-3xl leading-10 text-center text-gray-400 lg:w-5/6 lg:pb-0 lg:text-3xl lg:text-left lg:p-0 lg:pl-0 lg:pr-0">{description}</p>
           </div>
           <div className="ml-10 lg:ml-0 lg:w-3/6 min-h-60 ">
-            <img className=" lg:-mt-24 mb-12 mx-auto lg:-mb-20 py-24" src={img} alt=""/>
-            
+              <img className=" lg:-mt-24 mb-12 mx-auto lg:-mb-20 py-24 w-2/4" src={img} alt=""/>
           </div>
             
         </div>
